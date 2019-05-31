d685036199d459e81a34bcec292a00bbcab3975b
victtorsilvawd <victorsilvabstronger@gmail.com>
2019-05-31 14:28:40 -0300

    ajust

--- modulos/js/funcoes_pagina_categoria.js
+++ modulos/js/funcoes_pagina_categoria.js
@@ -86,5 +86,70 @@ class PaginaCategoria{
 
     }
 
+    static palavrasMaisBuscadas(arrayCategorias){
+    let catsLamp = arrayCategorias;
+    
+    catsLamp.forEach(id=>{
+
+    if($(`body.pagina-categoria.categoria-${id}`).length > 0 ){
+        
+        $(`<div class="box-palavras-chaves">
+            <div class="conteiner">
+            <strong class="text-center"><span class="title-mais-buscado">Palavras mais Buscadas</span></strong>
+            <div class="_wrap-bus-pl">
+            <div class="palavras-buscadas">
+            <ul class="list-unstyled">
+            <li><a class="mais-buscados" href="/lampada-led-5w?sort=mais_vendidos">lampada led 5w</a></li>
+            <li><a class="mais-buscados" href="/lampada-led-7w?sort=mais_vendidos">lampada led 7w</a></li>
+            <li><a class="mais-buscados" href="/lampada-led-15w?sort=mais_vendidos">lampada led 15w</a></li>
+            <li><a class="mais-buscados" href="/lampada-led-9w?sort=mais_vendidos">lampada led 9w</a></li>
+            <li><a class="mais-buscados" href="/lampada-led-12w?sort=mais_vendidos">lampada led 12w </a></li>
+            <li><a class="mais-buscados" href="/lampada-led-10w?sort=mais_vendidos">lampada led 10w </a></li>
+            <li><a class="mais-buscados" href="/lampada-led-rgb">lampada rgb</a></li>
+            </ul>
+
+
+            <ul class="list-unstyled">
+            <li><a class="mais-buscados" href="/kit-lampadas-de-led?sort=mais_vendidos">lampadas</a></li>
+            <li><a class="mais-buscados" href="/lampada-led-tubular?sort=mais_vendidos">lampada led tubular</a></li>
+            <li><a class="mais-buscados" href="/kit-lampadas-de-led?sort=mais_vendidos">lampadas led</a></li>
+            <li><a class="mais-buscados" href="/lampada-led?sort=mais_vendidos">lampada de led</a></li>
+            <li><a class="mais-buscados" href="/dicroica-led?sort=mais_vendidos">lampada dicroica</a></li>
+            <li><a class="mais-buscados" href="/led-automotivo?sort=mais_vendidos">lampada automotiva</a></li>
+            <li><a class="mais-buscados" href="/lampada-led-bolinha?sort=mais_vendidos">lampada bolinha 15w</a></li>
+
+            </ul>
+
+                
+            <ul class="list-unstyled">
+            <li><a class="mais-buscados" href="/lampada-bulbo-led-a60-12w-bivolt-branca-amarela-">Lampada bulbo 12w 4000k</a></li>
+            <li><a class="mais-buscados" href="/lampada-led-bulbo?sort=mais_vendidos">lampada churrasqueira</a></li>
+            <li><a class="mais-buscados" href="/lampada-led-rgb">lampada colorida</a></li>
+            <li><a class="mais-buscados" href="/lampada-led-wi-fi?sort=mais_vendidos">lampada de led wireless</a></li>
+            <li><a class="mais-buscados" href="/lampada-led-e27?sort=mais_vendidos">lampada e27</a></li>
+            <li><a class="mais-buscados" href="/lampada-led-g9?sort=mais_vendidos">lampada g9</a></li>
+            <li><a class="mais-buscados" href="/led-automotivo?sort=mais_vendidos">Lampada led automotiva</a></li>
+            </ul>
+
+                
+            <ul class="list-unstyled">
+            <li><a class="mais-buscados" href="/lampada-led-e14?sort=mais_vendidos">Lampada led e14</a></li>
+            <li><a class="mais-buscados" href="/lampada-led-tubular?sort=mais_vendidos">lampada led t8</a></li>
+            <li><a class="mais-buscados" href="/led-retro?sort=mais_vendidos">lampada retro</a></li>
+            <li><a class="mais-buscados" href=" /led-retro?sort=mais_vendidos">lampada vintage</a></li>
+            <li><a class="mais-buscados" href="/adaptador-soquete-led-e27-duplo">soquete duplo para lampada</a></li>
+            <li><a class="mais-buscados" href="/abracadeira-de-aco-para-lampada-led-tubular-t8-18w-e-36w">abracadeira em aco lampada led</a></li>
+            <li><a class="mais-buscados" href="/arandela-led?sort=mais_vendidos">arandela para lampada de bulbo</a></li>
+            </ul>
+
+            </div>
+            </div>
+            </div>
+            </div>`).appendTo(`body.pagina-categoria.categoria-${id} #corpo .conteudo.span9`);
+
+        }
+
+        });
+    }
 
 }
